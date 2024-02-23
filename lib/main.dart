326add9695f35771e33d4abd75b53ff7db09a939
ilhamsj <ilhamsaputrajati@gmail.com>
2024-02-23 16:57:49 +0700

    Add detail page with image and navigation icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cozy_marketplace/pages/detail_page.dart';
 import 'package:cozy_marketplace/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: const DetailPage(),
     );
   }
 }
--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -5,6 +5,90 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Container(
+                      height: 375,
+                      width: double.infinity,
+                      color: Colors.amber,
+                      child: Image.asset('assets/images/detail/pic.png',
+                          width: double.infinity,
+                          height: 375,
+                          fit: BoxFit.cover),
+                    ),
+                    Container(
+                      height: 40,
+                      width: double.infinity,
+                      color: Colors.transparent,
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 30,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            child: Center(
+                              child: Image.asset(
+                                'assets/images/navigations/Icon_left_outline.png',
+                                width: 20,
+                                height: 20,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(100),
+                              ),
+                            ),
+                            child: Center(
+                              child: Image.asset(
+                                'assets/images/navigations/Icon_love_outline.png',
+                                width: 20,
+                                height: 20,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 25,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Text('Hello world')
+          ],
+        ),
+      ),
+    );
   }
 }
