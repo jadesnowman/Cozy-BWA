08762213dc64d0c5a5befb0a927a46e8f9be47b2
ilhamsj <ilhamsaputrajati@gmail.com>
2024-02-23 16:26:31 +0700

    Add DetailPage and navigate to it from SpaceCard

--- lib/widgets/space_card.dart
+++ lib/widgets/space_card.dart
@@ -1,4 +1,5 @@
 import 'package:cozy_marketplace/models/index.dart';
+import 'package:cozy_marketplace/pages/detail_page.dart';
 import 'package:cozy_marketplace/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -9,71 +10,79 @@ class SpaceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(18),
-            child: Stack(
-              alignment: Alignment.topRight,
-              children: [
-                Image.asset(
-                  space.imageUrl,
-                  width: 130,
-                  height: 110,
-                  fit: BoxFit.cover,
-                ),
-                Container(
-                  width: 70,
-                  height: 30,
-                  decoration: BoxDecoration(
-                    color: purpleColor,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(36),
+    return InkWell(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => DetailPage(),
+        ),
+      ),
+      child: Row(
+        children: [
+          Container(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(18),
+              child: Stack(
+                alignment: Alignment.topRight,
+                children: [
+                  Image.asset(
+                    space.imageUrl,
+                    width: 130,
+                    height: 110,
+                    fit: BoxFit.cover,
+                  ),
+                  Container(
+                    width: 70,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: purpleColor,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(36),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/Icon_star_solid.png',
+                          width: 18,
+                          height: 18,
+                        ),
+                        Text(
+                          '1/3',
+                          style: heading1Light.copyWith(fontSize: 13),
+                        )
+                      ],
                     ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/images/Icon_star_solid.png',
-                        width: 18,
-                        height: 18,
-                      ),
-                      Text(
-                        '1/3',
-                        style: heading1Light.copyWith(fontSize: 13),
-                      )
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(space.name, style: heading1.copyWith(fontSize: 18)),
-            RichText(
-                text: TextSpan(
-              text: '\$${space.price}',
-              style: heading1.copyWith(fontSize: 16, color: purpleColor),
-              children: [
-                TextSpan(
-                  text: ' / month',
-                  style: heading2.copyWith(fontSize: 16),
-                ),
-              ],
-            )),
-            SizedBox(height: 16),
-            Text(space.address, style: heading2.copyWith(fontSize: 14)),
-          ],
-        )
-      ],
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(space.name, style: heading1.copyWith(fontSize: 18)),
+              RichText(
+                  text: TextSpan(
+                text: '\$${space.price}',
+                style: heading1.copyWith(fontSize: 16, color: purpleColor),
+                children: [
+                  TextSpan(
+                    text: ' / month',
+                    style: heading2.copyWith(fontSize: 16),
+                  ),
+                ],
+              )),
+              SizedBox(height: 16),
+              Text(space.address, style: heading2.copyWith(fontSize: 14)),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
