3ecd8ae1320522eaa403e9d46e9cefce97ae5af4
ilhamsj <ilhamsaputrajati@gmail.com>
2024-02-23 10:51:11 +0700

    Add new files and update configurations

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,80 @@
+import 'package:cozy_marketplace/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      bottom: false,
+      child: Stack(children: [
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Image.asset(
+            'assets/images/house.png',
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            top: 50,
+            left: 30,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/logo.png'),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Text(
+                'Find Cozy House\nto Stay and Happy',
+                style: heading1.copyWith(
+                  fontSize: 24,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Stop membuang banyak waktu\npada tempat yang tidak habitable',
+                style: heading2.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 40),
+              SizedBox(
+                width: 210,
+                height: 50,
+                child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: purpleColor,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(17),
+                      ),
+                    ),
+                    onPressed: () {
+                      print('Explore Now');
+                    },
+                    child: Text(
+                      'Explore Now',
+                      style: buttonTextStyle.copyWith(
+                        fontSize: 18,
+                      ),
+                    )),
+              )
+            ],
+          ),
+        ),
+      ]),
+    ));
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// homepage
+
+Color purpleColor = const Color(0xff5843BE);
+Color secondaryColor = const Color(0xffFFFFFF);
+
+Color textPrimaryColor = const Color(0xff000000);
+Color textSecondaryColor = const Color(0xff7A7E86);
+
+TextStyle heading1 = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: const Color(0xff000000),
+);
+
+TextStyle heading2 = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: const Color(0xff82868E),
+);
+
+TextStyle buttonTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: const Color(0xffFFFFFF),
+);
